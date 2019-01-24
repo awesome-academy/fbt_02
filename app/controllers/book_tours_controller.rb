cda5e3249cbdd0b43174abc226612c18960513aa
phambadat <phambadat.97@gmail.com>
2019-01-24 14:57:13 +0700

    booking tour

---
+++ app/controllers/book_tours_controller.rb
@@ -0,0 +1,38 @@
+class BookToursController < ApplicationController
+  before_action :load_tour, only: :show
+  before_action :load_user, only: :show
+
+  def create
+    @tour = BookTour.new tour_params
+    if @tour.save
+      flash[:success] = t "messenger.success_tour"
+      redirect_to tours_path
+    else
+      flash[:success] = t "messenger.book_fails"
+      render :show
+    end
+  end
+
+  def show; end
+
+  private
+
+  def load_tour
+    @tour = Tour.find_by id: params[:id]
+    return if @tour
+    flash[:danger] = t "messenger.no_data"
+    redirect_to tours_path
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "messenger.no_data"
+    redirect_to tours_path
+  end
+
+  def tour_params
+    params.require(:book_tour).permit(:user_id, :tour_id, :quantity,
+      :price, :note)
+  end
+end
