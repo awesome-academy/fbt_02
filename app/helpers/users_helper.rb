d44498a1b9c50f441b439366dd63443ab1e13c09
phambadat <phambadat.97@gmail.com>
2019-01-24 21:21:16 +0700

    signup now

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+require "digest"
+module UsersHelper
+  def gravatar_for user, size: Settings.avatar.size
+    md5 = Digest::MD5.new
+    gravatar_id = md5.hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+end
