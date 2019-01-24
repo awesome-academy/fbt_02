f1ef0d6cfe81c0fecc88e6a2adcf873bd71baada
phambadat <phambadat.97@gmail.com>
2019-01-24 21:21:16 +0700

    logout

--- app/controllers/book_tours_controller.rb
+++ app/controllers/book_tours_controller.rb
@@ -1,6 +1,5 @@
 class BookToursController < ApplicationController
   before_action :load_tour, only: :show
-  before_action :load_user, only: :show
 
   def create
     @tour = BookTour.new tour_params
@@ -24,15 +23,8 @@ class BookToursController < ApplicationController
     redirect_to tours_path
   end
 
-  def load_user
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t "messenger.no_data"
-    redirect_to tours_path
-  end
-
   def tour_params
-    params.require(:book_tour).permit(:user_id, :tour_id, :quantity,
-      :price, :note)
+    params.require(:book_tour).permit :user_id, :tour_id, :quantity,
+      :price, :note
   end
 end
