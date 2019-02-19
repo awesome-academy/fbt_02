7d2a4af38ed78cf5b369d29d62e712a49d558e10
phambadat <phambadat.97@gmail.com>
2019-02-19 10:37:49 +0700

    delete comment follow gem cancancan

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :load_comment, only: :destroy
+
   def create
     @comment = Comment.new comment_params
     if @comment.save
@@ -10,9 +12,26 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    if @comment.destroy
+      flash[:success] = t "comment.delete"
+      redirect_to tour_path @comment.tour_id
+    else
+      flash[:danger] = t "comment.fails"
+      redirect_to tour_path @comment.tour_id
+    end
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit :content, :user_id, :tour_id
+  end
+
+  def load_comment
+    @comment = Comment.find_by id: params[:id]
+    return if @comment
+    flash[:danger] = t "messenger.no_data"
+    redirect_to tour_path @comment.tour_id
   end
 end
